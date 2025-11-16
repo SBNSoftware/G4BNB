f843cc15dc54bc90704e1c00f515fc875add7aa2
kjplows <kom.plows@gmail.com>
2025-11-16 07:02:00 -0600

    Fix crash by clearing the container on end of event action. Mammoth changes, don't merge

--- include/NuBeamTrajectory.hh
+++ include/NuBeamTrajectory.hh
@@ -17,7 +17,8 @@
 
 class G4Polyline;                   // Forward declaration.
 
-typedef std::vector<G4VTrajectoryPoint*>  NuBeamTrajectoryPointContainer;
+//typedef std::vector<G4VTrajectoryPoint*>  NuBeamTrajectoryPointContainer;
+typedef std::vector<std::shared_ptr<G4TrajectoryPoint>>  NuBeamTrajectoryPointContainer;
 
 class NuBeamTrajectory : public G4VTrajectory
 {
@@ -28,8 +29,8 @@ public:
   NuBeamTrajectory(NuBeamTrajectory &);
   virtual ~NuBeamTrajectory();
 
-  inline void* operator new(size_t);
-  inline void  operator delete(void*);
+  //inline void* operator new(size_t);
+  //inline void  operator delete(void*);
   inline int operator == (const NuBeamTrajectory& right) const
   {return (this==&right);} 
   
@@ -218,12 +219,13 @@ public:
 
   virtual int GetPointEntries() const { return fPositionRecord.size(); }
   virtual G4VTrajectoryPoint* GetPoint(G4int i) const 
-  { return fPositionRecord[i]; }
+  { return (fPositionRecord[i]).get(); }
 };
 
 
 extern G4Allocator<NuBeamTrajectory> aTrajectoryAlloc;
 
+/*
 inline void* NuBeamTrajectory::operator new(size_t)
 {
   void* aTrajectory;
@@ -235,6 +237,7 @@ inline void NuBeamTrajectory::operator delete(void* aTrajectory)
 {
   aTrajectoryAlloc.FreeSingle((NuBeamTrajectory*)aTrajectory);
 }
+*/
 
 #endif
 
