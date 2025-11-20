81e38a3b9ca5aa2a6f42a997b5a33fe8a612f6af
Fran Nicolas <franciscojaviern1997@gmail.com>
2025-11-20 16:51:13 -0600

    Remove debug comment

--- src/BooNEHadronInelasticProcess.cc
+++ src/BooNEHadronInelasticProcess.cc
@@ -303,10 +303,7 @@ BooNEHadronInelasticProcess::PostStepDoIt(const G4Track& aTrack, const G4Step&)
 
     // Propagate QE info for secondaries created in BooNEpBeInteraction to the track info
     if (fInteraction->GetModelName() == "BooNEpBeInteraction") {
-      //std::cout << "    --> HadronInelasticProcess QE:" << fBooNEpBeModel->QuasiElasticStatus() << " for track E " << secTrack->GetKineticEnergy() << " PDG: " << secTrack->GetParticleDefinition()->GetPDGEncoding() << std::endl;
-
       tInfo->SetCreatorWasQE( fBooNEpBeModel->QuasiElasticStatus() );
-
     }
     else {
       tInfo->SetCreatorWasQE( false );
